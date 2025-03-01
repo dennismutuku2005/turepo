008185257a6608ca9bc613e3d0250e331e15e8d3
Dennis Muuo <dennismuuo.dev@gmail.com>
2025-03-01 13:31:13 +0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
