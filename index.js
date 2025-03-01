e61c6ea452ef4a9a7c72d6f9cff7a663fa31189b
Dennis Muuo <dennismuuo.dev@gmail.com>
2025-03-01 16:00:12 +0300

    first commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.post('/studentportal', async (req, res) => {
         const browser = await puppeteer.launch({
             headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+            executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : undefined,
         });
         const page = await browser.newPage();
 
